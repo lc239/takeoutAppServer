7424d4336bd02236e82a488ffd3dd1380cc019b8
lc239 <148198534+lc239@users.noreply.github.com>
2024-03-19 19:49:22 +0800

    Update StaticResourceFilter.java

--- src/main/java/com/lc/takeoutApp/filter/StaticResourceFilter.java
+++ src/main/java/com/lc/takeoutApp/filter/StaticResourceFilter.java
@@ -15,17 +15,11 @@ import reactor.core.publisher.Mono;
 public class StaticResourceFilter implements WebFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        System.out.println(1);
-        System.out.println(exchange.getRequest().getPath().pathWithinApplication());
 //        if (exchange.getRequest().getURI().getPath().equals("/")) {
 //            return Mono.defer(() -> ((DefaultWebFilterChain) chain).getHandler().handle(
 //                    exchange.mutate().request(exchange.getRequest().mutate().path("/a.html").build()).build()
 //            ));
 //        }
-        //...
-        if (exchange.getRequest().getPath().pathWithinApplication().toString().startsWith("/datealive")) {
-            return Mono.defer(() -> ((DefaultWebFilterChain) chain).getHandler().handle(exchange));
-        }
         //静态资源直接返回
         if(
                 exchange.getRequest().getPath().pathWithinApplication().toString().endsWith(".js") ||
